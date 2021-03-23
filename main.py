f4583bc492921dc1ab1352c634fc7483230f1925
gabrielapedreros <80491865+gabrielapedreros@users.noreply.github.com>
2021-03-23 16:32:22 -0400

    Add files via upload

---
+++ main.py
@@ -0,0 +1,4 @@
+user_choice_random_number = int(input("Enter a random number: "))          
+for user_choice_random_number in range(user_choice_random_number, 0, -1):
+    print(user_choice_random_number)
+5
